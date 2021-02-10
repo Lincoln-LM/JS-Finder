fe70712c5d05d8d29a153820f937f35656ecab75
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2021-02-09 21:25:11 -0700

    Update LCRNG.js

--- Core/LCRNG.js
+++ Core/LCRNG.js
@@ -1,1 +1,23 @@
-
+class PokeRNG {
+  constructor(seed) {
+    this.seed = BigInt(seed);
+    this.mult = BigInt(0x41C64E6D);
+    this.add = BigInt(0x6073);
+  }
+  
+  nextUInt() {
+    this.seed = (this.seed * this.mult + this.add) & BigInt(0xFFFFFFFF);
+    return Number(this.seed);
+  }
+  
+  nextUShort() {
+    return this.nextUInt() >>> 16;
+  }
+  
+  next(advances) {
+    for (let i=0; i<advances; i++) {
+      this.nextUInt();
+    }
+    return this.seed;
+  }
+}
