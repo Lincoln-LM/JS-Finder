b711c8e4adc4f87f95283287f3fa6907f195501d
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2021-02-09 21:25:37 -0700

    Update tools.js

--- Core/tools.js
+++ Core/tools.js
@@ -1,1 +1,9 @@
-
+function getIVs(iv1, iv2) {
+  hp = iv1 & 0x1f;
+  atk = (iv1 >>> 5) & 0x1f;
+  defense = (iv1 >>> 10) & 0x1f;
+  spa = (iv2 >>> 5) & 0x1f;
+  spd = (iv2 >>> 10) & 0x1f;
+  spe = iv2 & 0x1f;
+  return [hp, atk, defense, spa, spd, spe]
+}
