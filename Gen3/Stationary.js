dff7dee46e18771c4a889a607529c3493206e3c4
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2021-02-11 18:08:35 -0700

    adapt for general use, fix naming conventions, organize

--- Gen3/Stationary.js
+++ Gen3/Stationary.js
@@ -1,4 +1,3 @@
-
 function generate(initial,initialAdvances,maxAdvances,delay,tid,sid,ivn,ivx,targetNature,isShiny) {
   let natures = [ "Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Bold", "Docile", "Relaxed", "Impish", "Lax", "Timid", "Hasty", "Serious", "Jolly", "Naive", "Modest", "Mild", "Quiet", "Bashful", "Rash", "Calm", "Gentle", "Sassy", "Careful", "Quirky" ]
   
