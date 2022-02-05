d99d32e0c35ed2ea3911661d3f4aebeffc05b333
Lincoln-LM <maestaslincoln1@gmail.com>
2022-02-05 02:14:43 -0700

    use pknx dumps to complete spawner id list

---
+++ Resources/pla_spawners/convert_dump.py
@@ -0,0 +1,34 @@
+import json
+
+locations = ["obsidianfieldlands","crimsonmirelands","cobaltcoastlands","coronethighlands","alabastericelands"]
+prefixes = ["eve","fly","gmk","lnd","mas","oyb","swm","whl","sky"]
+
+for map_id in range(1,6):
+    markers = {}
+    with open(f"./dumps/Spawner_ha_area0{map_id}.txt",encoding="utf-8") as spawner_dump:
+        index = 0
+        for line in spawner_dump.readlines()[1:]:
+            split = line.split(",")
+            i = 0
+            while split[i][:4] != " V3f":
+                i += 1
+            pos = [float(x) for x in "".join(split[i:i+3])[5:-1].split(" ")]
+            flag = False
+            while not flag:
+                split2 = split[i].split('"')
+                if len(split2) == 3:
+                    flag = split2[-2][:3] in prefixes
+                i += 1
+            if split[i-1].split('"')[-2][:3] != "eve":
+                alpha = split[i-1].split('"')[-2][:3] == "oyb" or index == 0
+                markers[index*17] = {
+                    "coords": pos,
+                    "icon": "/pokearth/hisui/icons/alpha.png" if alpha else "/pokearth/hisui/icons/pokeball.png",
+                    "spawnerID": index*17,
+                    "ivs": 3 if alpha else 0,
+                    "name": split[i-1].split('"')[-2]
+                }
+                index += 1
+                # print(alpha,split[i-1].split('"')[-2],pos)
+    print(markers)
+    json.dump(markers,open(f"./jsons/{locations[map_id-1]}.json","w+"),indent=2)
