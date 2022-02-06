af2949e470063ab7038734598b48681236a25d2d
Lincoln-LM <maestaslincoln1@gmail.com>
2022-02-05 18:47:19 -0700

    create blacklist for alabastericelands

--- Resources/pla_spawners/convert_dump.py
+++ Resources/pla_spawners/convert_dump.py
@@ -2,9 +2,18 @@ import json
 
 locations = ["obsidianfieldlands","crimsonmirelands","cobaltcoastlands","coronethighlands","alabastericelands"]
 prefixes = ["eve","fly","gmk","lnd","mas","oyb","swm","whl","sky"]
+# bad system fix logic later
+blacklists = [
+    [],
+    [],
+    [],
+    [],
+    [1122,1428+17,1445+17+17,1530+17+17+17,1632+17+17+17+17,1683+17+17+17+17+17,1700+17+17+17+17+17+17,1734+17+17+17+17+17+17+17,1870+17+17+17+17+17+17+17+17,2856+17+17+17+17+17+17+17+17+17]
+]
 
 for map_id in range(1,6):
     markers = {}
+    offset = 0
     with open(f"./dumps/Spawner_ha_area0{map_id}.txt",encoding="utf-8") as spawner_dump:
         index = 0
         for line in spawner_dump.readlines()[1:]:
@@ -20,6 +29,13 @@ for map_id in range(1,6):
                     flag = split2[-2][:3] in prefixes
                 i += 1
             if split[i-1].split('"')[-2][:3] != "eve":
+                if index*17+offset in blacklists[map_id-1]:
+                    print(index*17, pos)
+                    # print(line)
+                    offset += 17
+                    # if offset == 0:
+                    #     offset += 17
+                    continue
                 alpha = split[i-1].split('"')[-2][4:6] == "ex" or index == 0
                 swarm = split[i-1].split('"')[-2][:3] == "mas"
                 markers[index*17] = {
@@ -31,5 +47,5 @@ for map_id in range(1,6):
                 }
                 index += 1
                 # print(alpha,split[i-1].split('"')[-2],pos)
-    print(markers)
+    # print(markers)
     json.dump(markers,open(f"./jsons/{locations[map_id-1]}.json","w+"),indent=2)
