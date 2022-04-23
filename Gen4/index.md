4635df5d8382e84113ba609f8b6307ce10ae6b8a
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2022-04-23 16:33:07 -0600

    basic dp battle tower prediction

--- Gen4/index.md
+++
@@ -1,4 +0,0 @@
-# Gen 4
-[**Stationary**](https://lincoln-lm.github.io/JS-Finder/Gen4/Stationary)
-
-[**Wild**](https://lincoln-lm.github.io/JS-Finder/Gen4/Wild)
--- JS/LCRNG.js
+++ JS/LCRNG.js
@@ -45,3 +45,35 @@ class PokeRNGR {
     return this.seed;
   }
 }
+
+class BattleTowerRNG {
+  constructor(seed) {
+    this.seed = BigInt(seed);
+    this.mult = BigInt(0x2E90EDD);
+    this.add = BigInt(0x1);
+  }
+  
+  nextUInt() {
+    this.seed = (this.seed * this.mult + this.add) & BigInt(0xFFFFFFFF);
+    return Number(this.seed);
+  }
+  
+  nextUShort() {
+    return Math.floor(this.nextUInt() / 65535);
+  }
+
+  rand(max) {
+    return this.nextUShort() % max;
+  }
+
+  randrange(min,max) {
+    return (this.nextUShort() % (max-min+1)) + min;
+  }
+  
+  next(advances=1) {
+    for (let i=0; i<advances; i++) {
+      this.nextUInt();
+    }
+    return this.seed;
+  }
+}
